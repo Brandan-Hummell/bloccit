7849b78a4584a8eadca9dff5e7cf591d7317dc8e
Brandan <bh2df@virginia.edu>
2017-12-05 00:11:14 -0500

    Assignment 23: Capitalized names before saving them

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,18 @@
 class User < ApplicationRecord
     before_save { self.email = email.downcase if email.present? }
+
+    before_save { 
+        if name.present?
+          name_array = self.name.split(' ')
+          name_array.each do |t|
+               t.capitalize!
+          end
+          self.name = name_array.join(' ')
+        end
+        }
     
 
-       validates :name, length: { minimum: 1, maximum: 100 }, presence: true
+      validates :name, length: { minimum: 1, maximum: 100 }, presence: true
 
       validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
       validates :password, length: { minimum: 6 }, allow_blank: true
