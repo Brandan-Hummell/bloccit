d6cfba9d0501724efe84c27e6808e089e555ce34
Brandan <bh2df@virginia.edu>
2017-12-10 15:43:31 -0500

    Assignment 26: Added some scopes for ordering posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,15 @@ class Post < ApplicationRecord
 
     default_scope { order('created_at DESC') }
 
+    def self.ordered_by_title 
+        order('title')
+    end
+
+    def self.ordered_by_reverse_created_at
+        order('created_at')
+    end
+    
+
     validates :title, length: { minimum: 5 }, presence: true
     validates :body, length: { minimum: 20 }, presence: true
     validates :topic, presence: true
